06d4a8c1a09219ef672b283429559ed484dd6bd8
M3D1T3 <sanchezpomares1306@gmail.com>
2023-08-07 15:21:03 +0200

    first commit

---
+++ registro.php
@@ -0,0 +1,29 @@
+<?php
+
+//Comprobamos si hemos recibido un archivo mediante POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['certificado'])) {
+    //Almacenamos el archivo en una variable
+    $certificado_tmp = $_FILES['certificado']['tmp_name'];
+
+    // Extraer los datos del certificado. Tendria que saber que tipo de certificado se usaria para poder 
+    //cambiar el sistema de validacion y extraccion del formulario
+    $cert_data = openssl_x509_parse(file_get_contents($certificado_tmp));
+    if (!$cert_data) {
+        die("Certificado no válido.");
+    }
+
+    // Tomar algún dato del certificado, por ejemplo, el email
+    $email_from_cert = isset($cert_data['subject']['emailAddress']) ? $cert_data['subject']['emailAddress'] : null;
+    if (!$email_from_cert) {
+        die("El certificado no tiene una dirección de correo electrónico válida.");
+    }
+
+    // Aquí insertarías en la base de datos los datos recogidos del certificado
+    // Por ejemplo:
+    // $conexion = new mysqli('host', 'usuario', 'contraseña', 'base_de_datos');
+    // $query = "INSERT INTO usuarios(email) VALUES ('{$email_from_cert}')";
+    // $conexion->query($query);
+
+    echo "Usuario registrado con éxito usando el email: {$email_from_cert}";
+}
+?>
